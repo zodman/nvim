c7eca8d29812f9bf348a64e8ac46a55c7803901c
Andres Vargas <avargas@nicoyalife.com>
2024-05-17 21:57:38 -0400

    Update plugins and configurations

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -31,4 +31,21 @@ return {
       { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
     },
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = { eslint = {} },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
 }

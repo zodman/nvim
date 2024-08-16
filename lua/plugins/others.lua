c381601e32451523d41fc9b06ae5e81f850b7e2d
Andres Vargas <avargas@nicoyalife.com>
2024-08-15 23:04:35 -0400

    Update lazy-lock.json and configure plugins

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -15,33 +15,28 @@ return {
     end,
   },
 
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     format = { timeout_ms = 120000 },
-  --     servers = { eslint = {} },
-  --     setup = {
-  --       eslint = function()
-  --         require("lazyvim.util").lsp.on_attach(function(client)
-  --           if client.name == "eslint_d" then
-  --             client.server_capabilities.documentFormattingProvider = true
-  --           elseif client.name == "eslint" then
-  --             client.server_capabilities.documentFormattingProvider = false
-  --           elseif client.name == "vtsls" then
-  --             client.server_capabilities.documentFormattingProvider = false
-  --           elseif client.name == "tsserver" then
-  --             client.server_capabilities.documentFormattingProvider = false
-  --           end
-  --         end)
-  --       end,
-  --     },
-  --   },
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
   {
     "andythigpen/nvim-coverage",
     event = "VeryLazy",
+    config = true,
     opts = {
       auto_reload = true, -- automatically reload coverage file on changes
     },
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+    config = true,
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    version = "*",
+    config = true,
   },
 }
